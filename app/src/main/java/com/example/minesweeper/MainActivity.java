565194c64ec594ee077bc7f27db6e5418b118103
SylvainCTY <sylvaincourty@gmail.com>
2020-12-08 09:11:38 +0100

    Initial fragment commit

--- app/src/main/java/com/example/minesweeper/MainActivity.java
+++ app/src/main/java/com/example/minesweeper/MainActivity.java
@@ -2,13 +2,32 @@ package com.example.minesweeper;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button btnGame = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnGame= findViewById(R.id.btn_NewGame);
     }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        btnGame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, Game.class));
+            }
+        });
+
+    }
+
+
 }
