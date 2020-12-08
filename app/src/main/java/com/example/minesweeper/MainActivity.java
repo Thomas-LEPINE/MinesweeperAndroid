c5aa5ac23e5fb11aab989ca755ebd031a964aa65
Thomas-LEPINE <64126235+Thomas-LEPINE@users.noreply.github.com>
2020-12-08 12:14:21 +0100

    bouton difficulté

--- app/src/main/java/com/example/minesweeper/MainActivity.java
+++ app/src/main/java/com/example/minesweeper/MainActivity.java
@@ -8,18 +8,66 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    private Button btnGame = null;
+
+    /* COMPOSANTS */
+    private Button btnGame;
+    private Button btnChangeDifficulty;
+    private Button btnSetings;
+    private Button btnExit;
+    /* ##### */
+
+    /* Attributs */
+    private int difficulty;
+    /* ###### */
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btnGame= findViewById(R.id.btnNewGame);
+        btnGame = (Button) findViewById(R.id.btnNewGame);
+        btnChangeDifficulty = (Button) findViewById(R.id.btnChangeDifficulty);
+        btnSetings = (Button) findViewById(R.id.btnSetings);
+        btnExit = (Button) findViewById(R.id.btnExit);
+
+        // La difficultée est mise à facile au début
+        btnChangeDifficulty.setText(getString(R.string.difficulty) + " " + getString(R.string.difficulty_easy));
+        difficulty = 0;
     }
 
 
     @Override
     protected void onStart() {
         super.onStart();
+
+        btnChangeDifficulty.setOnClickListener(new View.OnClickListener() { // Bouton difficulté
+            @Override
+            public void onClick(View v) {
+                switch(difficulty){
+                    case 0:
+                        // Facile -> normal
+                        btnChangeDifficulty.setText(getString(R.string.difficulty) + " " + getString(R.string.difficulty_normal));
+                        difficulty = 1;
+                        break;
+                    case 1:
+                        // normal -> difficile
+                        btnChangeDifficulty.setText(getString(R.string.difficulty) + " " + getString(R.string.difficulty_difficile));
+                        difficulty = 2;
+                        break;
+                    case 2:
+                        // difficile -> facile
+                        btnChangeDifficulty.setText(getString(R.string.difficulty) + " " + getString(R.string.difficulty_easy));
+                        difficulty = 0;
+                        break;
+                    default:
+                        // ? (bug) -> facile
+                        btnChangeDifficulty.setText(getString(R.string.difficulty) + " " + getString(R.string.difficulty_easy));
+                        difficulty = 0;
+                        break;
+                }
+            }
+        });
+
         btnGame.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
