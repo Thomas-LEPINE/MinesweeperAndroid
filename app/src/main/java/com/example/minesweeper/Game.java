028f35c7e5d37d960886f6900430cf4fc5079ae9
SylvainCTY <sylvaincourty@gmail.com>
2020-12-08 12:20:34 +0100

    Update Frag

--- app/src/main/java/com/example/minesweeper/Game.java
+++ app/src/main/java/com/example/minesweeper/Game.java
@@ -1,11 +1,16 @@
 package com.example.minesweeper;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AbsListView;
 import android.widget.Button;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Game extends AppCompatActivity {
 
@@ -16,6 +21,20 @@ public class Game extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
         btnBackMenu=findViewById(R.id.btnBackMenu);
+        List<Hexa> setOfMines = new ArrayList<>();
+        Coordinator  coor = new Coordinator(this,setOfMines);
+        setOfMines.add(new Hexa(1,2));
+        setOfMines.add(new Hexa(2,4));
+        getSupportFragmentManager().beginTransaction()
+                .add(R.id.lyhHexa,setOfMines.get(0))
+                .commit();
+        getSupportFragmentManager().beginTransaction()
+                .add(R.id.lyhHexa,setOfMines.get(1))
+                .commit();
+
+        RecyclerView hexaRcv;
+        //hexaRcv = (RecyclerView)  findViewById(R.id.hexa_rcv);
+        //GridLayoutManager manager = new GridLayoutManager(this, 3);
     }
 
     @Override
@@ -27,5 +46,9 @@ public class Game extends AppCompatActivity {
                 finish();
             }
         });
+
+
+
+
     }
 }
--- app/src/main/java/com/example/minesweeper/Hexa.java
+++ app/src/main/java/com/example/minesweeper/Hexa.java
@@ -1,5 +1,6 @@
 package com.example.minesweeper;
 
+import android.media.Image;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,58 +8,65 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Toast;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link Hexa#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class Hexa extends Fragment {
+    private int _row;
+    private int _col;
+    private int _state;
+    private int _nbbombes;
+    private ImageButton _ivMine;
+    private Coordinator _coor;
+    private View v;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public Hexa() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Hexa.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static Hexa newInstance(String param1, String param2) {
-        Hexa fragment = new Hexa();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+    public Hexa(int r, int c) {
+        this._row=r;
+        this._col=c;
     }
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
+
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_hexa, container, false);
+        v = inflater.inflate(R.layout.fragment_hexa,container, false);
+        _ivMine = (ImageButton) v.findViewById(R.id.imageButton);
+
+        _ivMine.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity(), "You Clicked the button!"+ String.valueOf(_col), Toast.LENGTH_LONG).show();
+
+            }
+        });
+
+
+
+        return v;
+    }
+
+    public void linkTogether(Coordinator c) {
+        _coor=c;
+    }
+
+
+    public void test() {
+        System.out.println(String.valueOf(_row)+"   "+String.valueOf(_col));
     }
 }
