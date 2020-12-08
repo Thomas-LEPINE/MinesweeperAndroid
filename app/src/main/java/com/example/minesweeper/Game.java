5140dfc68650ed4a4ea9650a39104ff4ff48b1d1
SylvainCTY <sylvaincourty@gmail.com>
2020-12-08 09:31:36 +0100

    Update Fragment

--- app/src/main/java/com/example/minesweeper/Game.java
+++ app/src/main/java/com/example/minesweeper/Game.java
@@ -2,13 +2,30 @@ package com.example.minesweeper;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Game extends AppCompatActivity {
+
+    private Button btnBackMenu = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+        btnBackMenu=findViewById(R.id.btnBackMenu);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        btnBackMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
