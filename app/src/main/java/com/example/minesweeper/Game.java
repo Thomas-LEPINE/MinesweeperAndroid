03857394788ede858c4be90642ff7c3304086e33
Thomas-LEPINE <64126235+Thomas-LEPINE@users.noreply.github.com>
2021-01-06 11:00:09 +0100

    modif

--- app/src/main/java/com/example/minesweeper/Game.java
+++ app/src/main/java/com/example/minesweeper/Game.java
@@ -24,29 +24,6 @@ public class Game extends AppCompatActivity {
         setContentView(R.layout.activity_game);
         btnBackMenu=findViewById(R.id.btnBackMenu);
         tvTimer = findViewById(R.id.tvTimer);
-        
-
-        Thread t = new Thread() {
-            @Override
-            public void run() {
-                while (!isInterrupted()) {
-                    try {
-                        Thread.sleep(1000);  //1000ms = 1 sec
-
-                        runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
-                                countTimer++;
-                                tvTimer.setText(String.valueOf(countTimer));
-                            }
-                        });
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        };
-        t.start();
     }
 
     @Override
--- app/src/main/java/com/example/minesweeper/MainActivity.java
+++ app/src/main/java/com/example/minesweeper/MainActivity.java
@@ -81,7 +81,10 @@ public class MainActivity extends AppCompatActivity {
         btnGame.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, Game.class));
+                Bundle bundleGame = new Bundle();
+                Intent intentGame = new Intent(MainActivity.this, Credit.class);  //Game
+                // intentGame.putExtras(bundleGame.putString(difficulty, int));
+                startActivity(intentGame);
             }
         });
 
