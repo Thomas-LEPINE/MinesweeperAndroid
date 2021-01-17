dd1f2e02a0dcbef099c149d7b210ed928677038e
Caitlin Jolivet <cait.jolivet@gmail.com>
2021-01-17 11:28:06 +0100

    ajout de la musique

---
+++ app/src/main/java/com/example/minesweeper/Music.java
@@ -0,0 +1,97 @@
+package com.example.minesweeper;
+
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Binder;
+import android.os.IBinder;
+
+import androidx.annotation.Nullable;
+
+public class Music extends Service {
+    private MediaPlayer mp;
+    private final IBinder mBinder=new MyServiceBinder();
+    public static Boolean serviceIsRunning = false;
+
+    //Methode permettant au client d'intéragir avec le service
+    public class MyServiceBinder extends Binder{
+        public Music getService()
+        {return Music.this;}
+    }
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return mBinder;
+    }
+    @Override
+    public boolean onUnbind(Intent intent) {
+        return super.onUnbind(intent);
+    }
+
+
+    @Override
+    public void onCreate (){
+        super.onCreate();
+        //Préparation de la musique
+        mp = MediaPlayer.create(this, R.raw.music1);
+        if(mp!= null) {
+            mp.setLooping(true);//musique jouée en boucle
+            mp.setVolume(100, 100);
+        }
+
+    }
+
+    @Override
+    public int onStartCommand (Intent intent, int flags, int startId)
+    {
+        serviceIsRunning=true;
+        mp.start();
+        return START_NOT_STICKY;//Ne pas relancer le service une fois kill
+    }
+    public void pauseMusic()
+    {
+        if(mp.isPlaying())
+        {
+            mp.pause();
+
+        }
+    }
+
+    public void resumeMusic()
+    {
+        if(!mp.isPlaying())
+        {
+            mp.start();
+        }
+    }
+
+    public void stopMusic()
+    {if(mp.isPlaying()) {
+        mp.stop();
+        mp.release();
+        mp = null;
+    }
+    }
+
+    @Override
+    public void onDestroy ()
+    {
+        super.onDestroy();
+        if(mp != null)
+        {
+            try{
+                serviceIsRunning=false;
+                mp.stop();
+                mp.release();
+            }finally {
+                mp = null;
+            }
+        }
+    }
+
+    public Boolean isMusicPlaying()
+    {
+        return mp.isPlaying();
+    }
+
+}
--- app/src/main/java/com/example/minesweeper/Settings.java
+++ app/src/main/java/com/example/minesweeper/Settings.java
@@ -3,21 +3,64 @@ package com.example.minesweeper;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Dialog;
+import android.app.Service;
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.media.MediaPlayer;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.preference.PreferenceManager;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import static android.preference.PreferenceManager.getDefaultSharedPreferences;
 
 public class Settings extends AppCompatActivity {
 
     private Button btnChangeUsername;
     private Button btnBackToMenu;
+    private Button btnOnOffMusic;
     private Dialog popup;
+    private String btnMusicString;
+    private Music musicThread;
+    protected Boolean musicIsOn;
+    private SharedPreferences myPreference ;
+    private SharedPreferences.Editor myEditor;
+    private ServiceConnection mServiceCon=new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
+
+            Music.MyServiceBinder myServiceBinder= (Music.MyServiceBinder) iBinder;
+            musicThread = myServiceBinder.getService();
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName componentName) {
+            musicThread=null;
+
+        }
+    };
+    void doBindService(){
+        bindService(new Intent(this,Music.class),
+                mServiceCon, Context.BIND_AUTO_CREATE);
+    }
+
+    void doUnbindService()
+    {
+        if(musicIsOn)
+        {
+            unbindService(mServiceCon);
+
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,28 +68,64 @@ public class Settings extends AppCompatActivity {
         setContentView(R.layout.activity_settings);
         btnChangeUsername = (Button) findViewById(R.id.btnChangeUsername);
         btnBackToMenu = (Button) findViewById(R.id.btnSettingsBackMenu);
-        popup=new Dialog(this);
+        btnOnOffMusic = (Button) findViewById(R.id.btnOnOffMusic);
+        popup = new Dialog(this);
+        myPreference = getDefaultSharedPreferences(getApplicationContext());
+        myEditor = myPreference.edit();
+
+        //Connection au service Music
+        doBindService();
+        //Récupération des variables permettant de définir l'état du bouton et de si la musique est on ou off
+        btnMusicString = myPreference.getString("btnMusicString", "Musique OFF");
+        musicIsOn = myPreference.getBoolean("musicIsOn", true);
+        btnOnOffMusic.setText(btnMusicString);
 
     }
 
     @Override
     protected void onStart() {
         super.onStart();
+
         btnChangeUsername.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 ShowUsernamePopup();
             }
         });
+
         btnBackToMenu.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //Sauvegarde de l'état du bouton et de la musique
+                myEditor.putBoolean("musicIsOn", musicIsOn);
+                myEditor.putString("btnMusicString", btnMusicString);
+                myEditor.apply();
                 startActivity(new Intent(Settings.this, MainActivity.class));
-
             }
         });
 
+        btnOnOffMusic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                musicIsOn = musicThread.isMusicPlaying();
+                //vérifie si la musique est éteinte ou allumé et changement d'état du bouton
+                    if (musicIsOn==true) {
+                        btnMusicString = "Musique ON";
+                        musicThread.pauseMusic();
+                        btnOnOffMusic.setText(btnMusicString);
+
+                    } else {
+                        btnMusicString = "Musique OFF";
+                        musicThread.resumeMusic();
+                        btnOnOffMusic.setText(btnMusicString);
+                    }
+            }
+
+
+        });
+
     }
+
     private void ShowUsernamePopup()
     {
         final EditText etUsername;
@@ -59,7 +138,7 @@ public class Settings extends AppCompatActivity {
         btnBackToSettings = (Button) popup.findViewById(R.id.btnUsernameBackSettings);
         btnValidateUsername = (Button) popup.findViewById(R.id.btnValidateUsername);
         etUsername = (EditText) popup.findViewById(R.id.etChangeUsername);
-        myPreference = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        myPreference = getDefaultSharedPreferences(getApplicationContext());
         myEditor = myPreference.edit();
 
         //Récupération de l'username et affichage dans l'editText
@@ -78,10 +157,10 @@ public class Settings extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                //Sauvegarde du nouveau username
-                myEditor.putString("username", etUsername.getText().toString());
-                myEditor.apply();
-                popup.cancel();
+        //Sauvegarde du nouveau username
+        myEditor.putString("username", etUsername.getText().toString());
+        myEditor.apply();
+        popup.cancel();
 
             }
         });
